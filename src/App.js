d5b58667b2eb9b5142cda24804c57b737e40d3bd
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-05-16 17:30:08 -0700

    fetch and search now working

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { getMonsters } from './services/fetch-utils';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [monsters, setMonsters] = useState();
+  const [monsters, setMonsters] = useState([]);
+  const [search, setSearch] = useState('pika');
 
   async function load() {
-    const data = await getMonsters();
+    const data = await getMonsters(search);
     console.log(data);
     setMonsters(data);
   }
@@ -15,10 +16,25 @@ function App() {
     load();
   }, []); // eslint-disable-line
 
+  async function handleSearch(e) {
+    e.preventDefault();
+
+    load();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <form onSubmit={handleSearch}>
+        <label>Search for a Pocket Monster: <input onChange={ e => setSearch(e.target.value)}/></label>
+      </form>
         
+      <header className="App-header">
+        {monsters.map(monster => <div key={monster._id}>
+          <p>{monster.pokemon}</p>
+          <p>hp: {monster.hp}</p>
+          <p>shape: {monster.shape}</p>
+          <img src={`${monster.url_image}`}/>
+        </div>)}
       </header>
     </div>
   );

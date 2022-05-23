5435c128aafe7a611d64f17840c6181e42012281
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-05-22 20:08:00 -0700

    added loading spinner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import { getMonsters } from './services/fetch-utils';
 import { useState, useEffect } from 'react';
+import LoadingSpinner from './LoadingSpinner';
 
 function App() {
   const [monsters, setMonsters] = useState([]);
   const [search, setSearch] = useState('pika');
+  const [loading, setLoading] = useState(false);
 
   async function load() {
+    setLoading(true);
     const data = await getMonsters(search);
     setMonsters(data);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -18,7 +22,7 @@ function App() {
   async function handleSearch(e) {
     e.preventDefault();
 
-    load();
+    await load();
   }
 
   return (
@@ -26,15 +30,17 @@ function App() {
       <form onSubmit={handleSearch}>
         <label>Search for a Pocket Monster: <input onChange={ e => setSearch(e.target.value)}/></label>
       </form>
+      { loading ? <LoadingSpinner /> : 
+        <header className="App-header">
+          {monsters.map(monster => <div key={monster._id}>
+            <p>{monster.pokemon}</p>
+            <p>hp: {monster.hp}</p>
+            <p>shape: {monster.shape}</p>
+            <img src={`${monster.url_image}`}/>
+          </div>)}
         
-      <header className="App-header">
-        {monsters.map(monster => <div key={monster._id}>
-          <p>{monster.pokemon}</p>
-          <p>hp: {monster.hp}</p>
-          <p>shape: {monster.shape}</p>
-          <img src={`${monster.url_image}`}/>
-        </div>)}
-      </header>
+        </header>
+      }
     </div>
   );
 }
